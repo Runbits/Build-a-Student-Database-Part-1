0217e9eb2973900e3638c2f163bcdc323dff98b7
Ricardo Martínez <50782028+Runbits@users.noreply.github.com>
2025-03-23 19:30:43 +0000

    done

--- insert_data.sh
+++ insert_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
 # Script to insert data from courses.csv and students.csv into students database
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
-echo echo $($PSQL "TRUNCATE students, majors, courses, majors_courses RESTART IDENTITY")
-sleep 1
-echo -e "\nAddign data of Major's & Courses\n"
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
+
 cat courses.csv | while IFS="," read MAJOR COURSE
 do
   if [[ $MAJOR != "major" ]]
@@ -25,8 +25,10 @@ do
       # get new major_id
       MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     fi
+
     # get course_id
     COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+
     # if not found
     if [[ -z $COURSE_ID ]]
     then
@@ -36,36 +38,39 @@ do
       then
         echo Inserted into courses, $COURSE
       fi
+
       # get new course_id
       COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
     fi
+
     # insert into majors_courses
     INSERT_MAJORS_COURSES_RESULT=$($PSQL "INSERT INTO majors_courses(major_id, course_id) VALUES($MAJOR_ID, $COURSE_ID)")
     if [[ $INSERT_MAJORS_COURSES_RESULT == "INSERT 0 1" ]]
-      then
-        echo "Inserted into majors_courses, $MAJOR : $COURSE"
+    then
+      echo Inserted into majors_courses, $MAJOR : $COURSE
     fi
   fi
 done
-sleep 1
-echo -e "\nAddign data of Student's\n"
+
 cat students.csv | while IFS="," read FIRST LAST MAJOR GPA
 do
   if [[ $FIRST != "first_name" ]]
   then
-  #get major_id
-  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-    #if not found
+    # get major_id
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'") 
+
+    # if not found
     if [[ -z $MAJOR_ID ]]
     then
-    #set to null
-    MAJOR_ID=null
+      # set to null
+      MAJOR_ID=null
     fi
-  #insert student
-  INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
+
+    # insert student
+    INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
     if [[ $INSERT_STUDENT_RESULT == "INSERT 0 1" ]]
     then
-    echo "Inserted into students, $FIRST $LAST"
+      echo Inserted into students, $FIRST $LAST
     fi
   fi
 done
